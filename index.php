f1d714e93e8fd3603871cc0779ae22fa4d0ebc5e
LidianeMara <lidianemara93@gmail.com>
2019-05-20 18:35:07 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!--PHP SITES DINAMICOS -->
+<!-- -->
+<!DOCTYPE html>
+<html lang = "pt-br">
+<head>	
+	<meta charset = "utf-8">
+	<title> Título da Página </title>
+</head>
+<body> 
+	
+    <?php #inicio do código em PHP
+        $titulo = '<h1> Página Padrão PHP </h1>'; 
+        echo( $titulo);
+
+    
+
+    ?> <!-- fim do código em PHP -->
+</body>
+</html> 
