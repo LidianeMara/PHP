816b8ef3d8e2568b60ae0ccc9915f2bfcb9bd024
LidianeMara <lidianemara93@gmail.com>
2019-06-04 17:04:26 -0300

    add files index

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
-<!--PHP SITES DINAMICOS -->
-<!-- -->
-<!DOCTYPE html>
-<html lang = "pt-br">
-<head>	
-	<meta charset = "utf-8">
-	<title> Título da Página </title>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Tijolo Criativo </title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Consultoria e Marketing Digital" />
+<meta name="keywords" content="marketing digital consultoria coloborativo">
+<meta name="autor" content="Lidiane Mara">
+<link rel="shortcut icon" type="imagens/x-icon" href="favicon.ico" />
+<link rel="stylesheet" href="css/folha_all.css">
+<link rel="stylesheet" href="css/home.css">
 </head>
-<body> 
+
+<body>
 	
-    <?php #inicio do código em PHP
-        $titulo = '<h1> Página Padrão PHP </h1>'; 
-        echo( $titulo);
-    
+<header>
+	<img src="imagens/topocurso.png" alt="">
+	
+	<nav>
+		<h1>Tijolo Criativo</h1>
+		<p>
+		<a href="#">Início</a>
+		</p>
+	</nav>
+	</header>
+	<main>
+		<article>
+			<h1>Tijolo Criativo</h1>
+			<hr>
+			<p>Mussum Ipsum, cacilds vidis litro abertis. Manduma pindureta quium dia nois paga. Per aumento de cachacis, eu reclamis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.</p>
 
-    
+			<p>Leite de capivaris, leite de mula manquis sem cabeça. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Suco de cevadiss deixa as pessoas mais interessantis. Diuretics paradis num copo é motivis de denguis.</p>
 
-    ?> <!-- fim do código em PHP -->
+			<p>Mé faiz elementum girarzis, nisi eros vermeio. Viva Forevis aptent taciti sociosqu ad litora torquent. Casamentiss faiz malandris se pirulitá. Atirei o pau no gatis, per gatis num morreus.</p>
+		</article>
+	</main>
+	<footer>
+		<p>Site desenvolvido por Lidiane Mara</p>
+	</footer>
 </body>
-</html> 
+</html>
